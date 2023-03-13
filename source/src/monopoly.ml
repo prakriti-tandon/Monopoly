7098856cf794a70d2e948f511a0cb8c7d4f9a2a2
Lauren Douglass <led94@cornell.edu>
2023-03-13 15:03:50 -0400

    Created board json and monopoly type t - still may tweak t and json depending on json parsing

--- source/src/monopoly.ml
+++ source/src/monopoly.ml
@@ -1,4 +1,23 @@
-type t = unit
+type property = {
+  id : string;
+  price : int;
+  rent : int;
+  owner : string;
+}
+
+type space_type =
+  | Go of { salary : int }
+  | Property of property
+
+type space = {
+  space_number : int;
+  info : space_type;
+}
+
+type t = {
+  spaces : space list;
+  num_spaces : int;
+}
 
 let from_json json = raise (Failure "Unimplemented: Monopoly.from_json")
 let owner player = raise (Failure "Unimplemented: Monopoly.owner")
@@ -6,6 +25,8 @@ let owner player = raise (Failure "Unimplemented: Monopoly.owner")
 let set_owner property player mon =
   raise (Failure "Unimplemented: Monopoly.set_owner")
 
+let name property = raise (Failure "Unimplemented: Monopoly.name")
 let description property = raise (Failure "Unimplemented: Monopoly.description")
 let price property = raise (Failure "Unimplemented: Monopoly.price")
 let rent property = raise (Failure "Unimplemented: Monopoly.rent")
+let space_type property = raise (Failure "Unimplemented: Monopoly.type")
--- source/src/monopoly.mli
+++ source/src/monopoly.mli
@@ -11,19 +11,26 @@ type t
 val from_json : Yojson.Basic.t -> t
 (** [from_json j] initializes the monopoly board from file [j]. *)
 
-val owner : string -> string
+val owner : int -> string
 (** [owner p] is the name of the player that owns the property at space [p] or
     None if no player owns property [p]. *)
 
-val set_owner : string -> string -> t -> t
+val set_owner : int -> string -> t -> t
 (** [owner p u m] is a the monopoly board [m] such that player [u] owns the
     property at space [p]. *)
 
-val description : string -> string
-(** [description p] is the description of the property at space p. *)
+val name : int -> string
+(** [description p] is the name of the property or other space at space [p]. *)
 
-val price : string -> int
-(** [price p] is the price to purchase the property at space p. *)
+val description : int -> string
+(** [description p] is the description of the property at space [p]. *)
 
-val rent : string -> int
-(** [rent p] is the rent price for the property at space p. *)
+val price : int -> int
+(** [price p] is the price to purchase the property at space [p]. *)
+
+val rent : int -> int
+(** [rent p] is the rent price for the property at space [p]. *)
+
+val space_type : int -> string
+(** [space_type p] is the type of space p. As of MS1, this can either be
+    "property" or "go". *)
