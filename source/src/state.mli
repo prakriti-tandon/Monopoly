8bb0216eb3677c3715a16a0f75923149f52a8b61
pt293 <pt293@cornell.edu>
2023-03-14 23:30:10 -0400

    Implement state.ml
    - Implemented init_state, name, current_pos, change_owns and go

--- source/src/state.mli
+++ source/src/state.mli
@@ -5,6 +5,15 @@
 type t
 (** The abstract type of values representing the player state. *)
 
+type combined_state
+(**The abstract type of the combined state of player1, player2, and monopoly
+   game*)
+
+(**The type represents the result of a buy or pay_rent action*)
+type result =
+  | Illegal
+  | Legal of combined_state
+
 val init_state : string -> t
 (**[init_state str] is the initial state of the player with name [str]. In that
    state, the player's current position is the go position. They have 500
@@ -24,3 +33,11 @@ val go : t -> Monopoly.t -> t
 (**[go player] is the new state of the [player] after they have rolled the dice
    and moved ahead in the game. [go player] only affects the current position of
    the [player] *)
+
+val pay_rent : t -> t -> Monopoly.t -> result
+(**[pay_rent player1 player2 game] is the result of [player1] paying rent to
+   [player2] in the Monopoly [game]
+
+   - If player1 has insufficient balance, result is [Illegal]
+   - Otherwise, result is a [Legal combined_result] with new player1' , new
+     player2' and same game*)
