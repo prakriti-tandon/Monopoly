2e74cba7bc7c9484cca3bac2e10a22b4aefd8b26
Lauren Douglass <led94@cornell.edu>
2023-03-14 21:55:43 -0400

    Added implementations for monopoly.ml - tweaked .mli to include Monopoly.t type as a function input - having an issue with test file in my vscode

--- source/src/monopoly.ml
+++ source/src/monopoly.ml
@@ -1,5 +1,12 @@
+open Yojson.Basic.Util
+
+exception UnknownSpace
+exception SpaceNotOwnable
+exception DescriptionNotAvailable
+
 type property = {
   id : string;
+  description : string;
   price : int;
   rent : int;
   owner : string;
@@ -22,17 +29,70 @@ type t = {
   num_spaces : int;
 }
 
-let from_json json = raise (Failure "Unimplemented: Monopoly.from_json")
-let owner player = raise (Failure "Unimplemented: Monopoly.owner")
+let property_of_json json =
+  {
+    id = json |> member "id" |> to_string;
+    description = json |> member "description" |> to_string;
+    price = json |> member "price" |> to_int;
+    rent = json |> member "rent" |> to_int;
+    owner = json |> member "property" |> to_string;
+  }
 
-let set_owner property player mon =
+let info_of_json json =
+  let stype = json |> member "type" |> to_string in
+  if stype = "go" then
+    Go
+      {
+        id = json |> member "name" |> to_string;
+        salary = json |> member "salary" |> to_int;
+      }
+  else Property (property_of_json json)
+
+let space_of_json json =
+  { space_number = json |> member "space" |> to_int; info = info_of_json json }
+
+let from_json json =
+  {
+    spaces = json |> member "spaces" |> to_list |> List.map space_of_json;
+    num_spaces = json |> member "number_spaces" |> to_int;
+  }
+
+let find_space mon property =
+  match List.filter (fun x -> x.space_number = property) mon.spaces with
+  | [ sp ] -> sp
+  | _ -> raise UnknownSpace
+
+let owner mon property =
+  match (find_space mon property).info with
+  | Go a -> raise SpaceNotOwnable
+  | Property b -> if b.owner = "" then None else Some b.owner
+
+let set_owner mon property player =
   raise (Failure "Unimplemented: Monopoly.set_owner")
 
-let name property = raise (Failure "Unimplemented: Monopoly.name")
-let description property = raise (Failure "Unimplemented: Monopoly.description")
-let price property = raise (Failure "Unimplemented: Monopoly.price")
-let rent property = raise (Failure "Unimplemented: Monopoly.rent")
-let space_type property = raise (Failure "Unimplemented: Monopoly.type")
+let name mon property =
+  match (find_space mon property).info with
+  | Go a -> a.id
+  | Property b -> b.id
 
-let number_of_spaces game =
-  raise (Failure "Unimplemented: Monopoly.number_of_spaces")
+let description mon property =
+  match (find_space mon property).info with
+  | Go a -> raise DescriptionNotAvailable
+  | Property b -> b.description
+
+let price mon property =
+  match (find_space mon property).info with
+  | Go a -> raise SpaceNotOwnable
+  | Property b -> b.price
+
+let rent mon property =
+  match (find_space mon property).info with
+  | Go a -> raise SpaceNotOwnable
+  | Property b -> b.rent
+
+let space_type mon property =
+  match (find_space mon property).info with
+  | Go a -> "go"
+  | Property b -> "property"
+
+let number_of_spaces game = game.num_spaces
--- source/test/main.ml
+++ source/test/main.ml
@@ -1,1 +1,22 @@
-(* unimplemented*)
+open OUnit2
+open Game
+open Monopoly
+open Command
+open State
+open Yojson
+
+let board = Monopoly.from_json (Yojson.Basic.from_file "data/board.json")
+
+let owner_test (name : string) (board : Monopoly.t) (space : int)
+    (expected_output : string) : test =
+  name >:: fun _ -> assert_equal expected_output (Monopoly.owner board space)
+
+let monopoly_tests = []
+let command_tests = []
+let state_tests = []
+
+let suite =
+  "test suite for final project"
+  >::: List.flatten [ monopoly_tests; command_tests; state_tests ]
+
+let _ = run_test_tt_main suite
