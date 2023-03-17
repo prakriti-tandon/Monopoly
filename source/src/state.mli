57df31d49df30fbe45539e84ead44a61e159ae11
imw25 <imw25@cornell.edu>
2023-03-16 22:03:36 -0700

    Added specifications for state.mli
    -Declared type of functions current_balance, change_balance, owns, and  buy_property
    -Wrote specfiications for each of the functions listed above
    -commit by Isabella Wells imw25@cornell.edu

--- source/src/state.mli
+++ source/src/state.mli
@@ -25,6 +25,22 @@ val name : t -> string
 val current_pos : t -> int
 (** [current_pos player] is the current position of the state [player]*)
 
+val current_balance : t -> int
+(**[current_balance player] is the balance/amount of money in the player's bank
+   account in their current state [player] *)
+
+val change_balance : t -> int -> t
+(**[change_balance player amt] is the new state of player's state [player] after
+   their balance is changed by [amt]. If [amt] is negative, money will be
+   deducted from the current balance in state [player] by [amt]. If [amt] is
+   negative, that means money will be added to the current balance in state
+   [player] by [amt]. *)
+
+val owns : t -> string -> Monopoly.t -> bool
+(**[owns player s game] is whether the [player] owns the property located at
+   space [s] in the [game]. Raises [SpaceNotOwnable] if [s] is not a valid
+   property in the [game]. *)
+
 val change_owns : int -> t -> t
 (**[change_owns pos player] is the new state of state [player] after property
    [pos] has been added to its set of owned properties*)
@@ -38,6 +54,13 @@ val pay_rent : t -> t -> Monopoly.t -> result
 (**[pay_rent player1 player2 game] is the result of [player1] paying rent to
    [player2] in the Monopoly [game]
 
-   - If player1 has insufficient balance, result is [Illegal]
+   - If [player1] has insufficient balance, result is [Illegal]
    - Otherwise, result is a [Legal combined_result] with new player1' , new
      player2' and same game*)
+
+val buy_property : t -> int -> Monopoly.t -> result
+(**[buy_property player space game] is the result of [player1] buying a property
+   located at [space] in the current [game]. -If the [player1] has insufficient
+   funds, result is [Illegal] -If the [space] is not a valid [space] in the
+   [game], result is [Illegal] -Otherwise, the result is [Legal combined_result]
+   with new player' and a new game *)
