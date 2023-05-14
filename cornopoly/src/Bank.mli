a162c2e95007ef8a1ae41d8915dd5fa7d3b04645
imw25 <imw25@cornell.edu>
2023-05-14 12:05:18 -0700

    Bank.mli
    -added function funds

--- cornopoly/src/Bank.mli
+++ cornopoly/src/Bank.mli
@@ -10,6 +10,9 @@ exception InsufBankFunds
 val init_bank : int -> t
 (** create a bank*)
 
+val funds : t -> int
+(*[funds bank] returns the current amt of funds in the [bank].*)
+
 val add_funds : t -> int -> unit
 (** [add_funds bank a] adds [a] amount of funds to [bank]. This function is used
     when buying a property, or repaying a loan, or any action that increases the
