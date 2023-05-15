90c13f2269883490aac60f0e3ba7de5dc6de153b
imw25 <imw25@cornell.edu>
2023-05-14 21:45:09 -0700

    excec_go
    -update the player state to decrease the num of days they have left in jail by one after passing go space

--- cornopoly/src/go.ml
+++ cornopoly/src/go.ml
@@ -1,3 +1,17 @@
 let exec_go pls curr_player board =
   let sal = Board.salary board 0 in
-  Property.update_player pls curr_player (State.change_balance curr_player sal)
+  let () =
+    Property.update_player pls curr_player
+      (State.change_balance curr_player sal)
+  in
+  let () =
+    let check_owes x =
+      match State.owes_to_bank x with
+      | Some x, y ->
+          Property.update_player pls curr_player
+            (State.turn_in_jail curr_player 1)
+      | None, _ -> ()
+    in
+    check_owes curr_player
+  in
+  ()
